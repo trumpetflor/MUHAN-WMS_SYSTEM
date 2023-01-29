a0d6b4f90babc69a8aa71ad57a551e00605b453e
sourgummy <118324239+sourgummy@users.noreply.github.com>
2023-01-29 23:54:25 +0900

    < 23/01/29 > Warehouse
    창고 등록, 창고 조회 완료
    root-context.xml : DB 경로 수정

--- MuhanSangsa/src/main/java/com/thisteam/muhansangsa/mapper/WarehouseMapper.java
+++ MuhanSangsa/src/main/java/com/thisteam/muhansangsa/mapper/WarehouseMapper.java
@@ -1,6 +1,16 @@
 package com.thisteam.muhansangsa.mapper;
 
+import java.util.List;
+
+import com.thisteam.muhansangsa.vo.WarehouseVO;
+
 public interface WarehouseMapper {
+	
+	// 창고 등록
+	int insertWarehouse(WarehouseVO warehouse);
+	
+	// 창고 조회
+	List<WarehouseVO> selectWarehouseList();
 
 }
 
