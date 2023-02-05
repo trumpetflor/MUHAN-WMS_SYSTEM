ccfc84e77dde1620a07dd40fc3f83631c4ebc31c
SwEclipse <121210254+SwEclipse@users.noreply.github.com>
2023-02-05 23:58:29 +0900

    < 230205 - sangwoo > 입고예정 작업현황 in_waiting_form / insert 뷰페이지 추가 맵핑작업 mybatis-config.xml - InVO alias 추가

--- MuhanSangsa/src/main/java/com/thisteam/muhansangsa/controller/InController.java
+++ MuhanSangsa/src/main/java/com/thisteam/muhansangsa/controller/InController.java
@@ -1,13 +1,69 @@
 package com.thisteam.muhansangsa.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.thisteam.muhansangsa.service.InService;
+import com.thisteam.muhansangsa.vo.ClientVO;
+import com.thisteam.muhansangsa.vo.EmployeesVO;
+import com.thisteam.muhansangsa.vo.InVO;
 
 @Controller
 public class InController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(InController.class);
+	
 	@Autowired
 	private InService service;
+	
+	
+	// ======================== yeram ==============================
+	
+	@GetMapping(value = "/InProcessing")
+	public String processing() {
+		return "in/in_processing";
+	}
+	
+	
+
+	
+	
+	
+	
+
+	
+	
+	// ======================== yeram ==============================
+	
+	// ======================== sangwoo ============================
+	@GetMapping(value = "/InSchedule")
+	public String waiting(Model model) {
+		List<InVO> inList = service.getInList();
+		model.addAttribute("inList", inList);
+		return "in/in_waiting_form";
+	}
+	
+	@GetMapping(value = "/InWaitingInsertForm")
+	public String waitingInsert(Model model) {
+		List<ClientVO> clientList = service.getClientList();
+		List<EmployeesVO> employeeList = service.getEmployeeList();
+		model.addAttribute("clientList",clientList);
+		model.addAttribute("empList", employeeList);
+		return "in/in_waiting_insert";
+	}
+	
+	
+	
+	
+	// ======================== sangwoo ============================
 }
--- MuhanSangsa/src/main/java/com/thisteam/muhansangsa/service/InService.java
+++ MuhanSangsa/src/main/java/com/thisteam/muhansangsa/service/InService.java
@@ -1,10 +1,15 @@
 package com.thisteam.muhansangsa.service;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thisteam.muhansangsa.mapper.InMapper;
+import com.thisteam.muhansangsa.vo.ClientVO;
+import com.thisteam.muhansangsa.vo.EmployeesVO;
+import com.thisteam.muhansangsa.vo.InVO;
 
 
 
@@ -14,6 +19,18 @@ public class InService {
 
 	@Autowired
 	private InMapper mapper;
+
+	public List<ClientVO> getClientList() {
+		return mapper.getClientList();
+	}
+
+	public List<EmployeesVO> getEmployeeList() {
+		return mapper.getEmployeeList();
+	}
+
+	public List<InVO> getInList() {
+		return mapper.getInList();
+	}
 	
 
 
